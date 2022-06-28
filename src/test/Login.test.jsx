32bda81c360934d3b96b0a3f341ca299c1df5e9a
moreirathais <thais_mjs@hotmail.com>
2022-06-28 16:55:13 -0300

    Teste página de login

--- src/test/Login.test.jsx
+++ src/test/Login.test.jsx
@@ -1,20 +1,30 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import Login from "../pages/Login";
-import { logedIn } from "../services/api"
+import { userLogin } from "../lib/authentication.js";
+import { MemoryRouter } from "react-router-dom";
 
-jest.mock("../services/api")
-jest.mock("react-router-dom")
+const mockedUsedNavigate = jest.fn();
+jest.mock("../lib/authentication.js")
+jest.mock("react-router-dom",()=>{
+  return {
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUsedNavigate,
+  }
+})
 
 describe("Login", () => {
   beforeEach(() => {
-   logedIn.mockClear();
+    userLogin.mockClear();
   });
   it("Deverá logar usuário corretamente", async () => {
-    logedIn.mockResolvedValueOnce({})
-    render(<Login />)
+    userLogin.mockResolvedValueOnce({})
+    render(
+    <MemoryRouter>
+    <Login />
+   </MemoryRouter>)
 
-    const email = "emplo@exemplo.com"
+    const email = "exemplo@exemplo.com"
     const password = "123456"
 
     const getEmail = screen.getByPlaceholderText("Email")
@@ -26,8 +36,8 @@ describe("Login", () => {
     user.click(btnLogin)
 
     await waitFor(() => {
-      expect(logedIn).toHaveBeenCalledWith(email, password)
+      expect(userLogin).toHaveBeenCalledWith(email, password)
     });
-    expect(logedIn).toHaveBeenCalledTimes(1)
+    expect(userLogin).toHaveBeenCalledTimes(1)
   })
 })
