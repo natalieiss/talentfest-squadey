fd5829361d12454b50feeea0e80b2431d08440cc
Jessika Mirella <jessikamirellabraga@gmail.com>
2022-06-28 22:13:31 -0300

    termos e condiçoes

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -22,7 +22,8 @@ function Historic() {
       <Header customClass="centralize" children="HISTÓRICO"/>
       <Button type="button" onClick={()=>{setIsmodalVisible(true)}}>Termos e Condições</Button>
       <Button onClick={handleOccurrance}>Aviso de Sinistro</Button>
-      {isModalVisible ? <Modal onClose={()=>{setIsmodalVisible(false)}}><h1>Termos</h1></Modal> : null} 
+      {isModalVisible ? <Modal onClose={()=>{setIsmodalVisible(false)}}>
+        <p>Declaro que todas as informações constantes neste formulário para fins de abertura de sinistro, são completas, verdadeiras e corretas em todos os detalhes.Tendo ciência que serão averiguadas e que arcarei com as consequências de afirmações inverídicas.</p></Modal> : null} 
     </Container>
   );
 }
