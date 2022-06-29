3680936d4a069b9b9fb285de39f5c2bacae29712
Adriana Tiemi Watanabe <new@AW.local>
2022-06-29 10:31:54 -0300

    Correção dos conflitos

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -9,25 +9,9 @@ import Footer from '../components/Footer';
 
 function History() {
   const [policy, setPolicy] = useState([]);
-  //const [userId, setUserId] = useState();
-
-  const id = `ID-00000324`;
-  const estado = "Solicitação Enviada";
-  const subEstado = "N/A";
-  const preco = 10000;
-  const tipo = "Colisão";
-  const idPolicy = "";
-
-  //authChange(() => setUserId(auth.currentUser.uid));
 
   useEffect(() => {
     async function showAllPolicy() {
-      // authChange(async (logged) => {
-      //   if(logged) {
-      //     const allPolicy = await getPolicy(auth.currentUser.uid);
-      //     return setPolicy(allPolicy);
-      //   }
-      // })
       const allPolicy = await getPolicy();
       setPolicy(allPolicy);
     };
